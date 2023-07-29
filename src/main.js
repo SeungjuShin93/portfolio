a8669828924fb67606255b62e7eb5f37ca8ebb94
Ted <shinsuengju1@gmail.com>
2023-07-29 16:11:13 +0900

    JS-menu

--- src/main.js
+++ src/main.js
@@ -16,12 +16,24 @@ document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-//
+// When scrolling down, make the arrow-up button transparent
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
   if (window.scrollY > homeHeight / 2) {
     arrowUp.style.opacity = 1;
+    arrowUp.style.cursor = 'pointer';
   } else {
     arrowUp.style.opacity = 0;
+    arrowUp.style.cursor = 'default';
   }
+});
+
+// Handle Navbar toggle button click
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
 });
